6f3ac6988a1708b1f763f341ea5500798d350b9d
forvoid <172953249@qq.com>
2018-11-06 19:43:56 +0800

    删除无用代码

--- src/main/java/com/forvoid/AOP/DataSourceInterceptor.java
+++ src/main/java/com/forvoid/AOP/DataSourceInterceptor.java
@@ -17,7 +17,8 @@ import org.springframework.stereotype.Component;
  */
 @Aspect
 @Component
-@Order(0) // execute before @Transactional 这里存在多个Aspect之间的一个执行顺序的问题，
+@Order(0)
+// execute before @Transactional 这里存在多个Aspect之间的一个执行顺序的问题，
 // 必须保证切换数据源的Aspect必须在@Transactional这个Aspect之前执行，
 // 所以这里使用了@Order(0)来保证切换数据源先于@Transactional执行
 public class DataSourceInterceptor {
